e8e88ccfbce5a6c7c365e8815fb5f7ee39d7be8b
Freddi <mail@FrederikHeinrich.de>
2025-08-02 19:41:11 +0200

    refactor(build): add Blossom plugin and enhance BuildInfo logging

--- src/main/java/modtemplate/ServerModTemplate.java
+++ src/main/java/modtemplate/ServerModTemplate.java
@@ -6,12 +6,14 @@ import org.slf4j.LoggerFactory;
 
 public class ServerModTemplate implements DedicatedServerModInitializer {
 
-    public static final String MOD_ID = "mod-template";
+    public static final String MOD_ID = BuildInfo.artifact();
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 
     @Override
     public void onInitializeServer() {
         LOGGER.info("Hello Fabric server world!");
+
+        LOGGER.info("BuildInfo: version={}, group={}, artifact={}, commit={}, branch={}", BuildInfo.version(), BuildInfo.group(), BuildInfo.artifact(), BuildInfo.commit(), BuildInfo.branch());
     }
 }
