6c61dc739511e6608c9b1a71b09aa5f8daf49fb7
Freddi <mail@FrederikHeinrich.de>
2025-08-02 21:00:32 +0200

    refactor(network): update HelloPacket for Minecraft version compatibility

--- src/main/java/modtemplate/ClientModTemplate.java
+++ src/main/java/modtemplate/ClientModTemplate.java
@@ -1,9 +1,10 @@
 package modtemplate;
 
-import modtemplate.net.HelloPacket;
 import net.fabricmc.api.ClientModInitializer;
-
+#if MC_VER >= MC_1_20_5
+import modtemplate.net.HelloPacket;
 import net.fabricmc.fabric.api.networking.v1.PayloadTypeRegistry;
+#endif
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -18,8 +19,11 @@ public class ClientModTemplate implements ClientModInitializer {
 
         LOGGER.info("BuildInfo: version={}, group={}, artifact={}, commit={}, branch={}, targetMCVersion={}", BuildInfo.version(), BuildInfo.group(), BuildInfo.artifact(), BuildInfo.commit(), BuildInfo.branch(), BuildInfo.mcVersion());
 
+        #if MC_VER >= MC_1_20_5
         PayloadTypeRegistry.playC2S().register(HelloPacket.ID, HelloPacket.STREAM_CODEC);
         PayloadTypeRegistry.playS2C().register(HelloPacket.ID, HelloPacket.STREAM_CODEC);
+        #else
 
+        #endif
     }
 }
