f2bba9ef1feb20b7eba242c5f744cc38b283c034
Freddi <mail@FrederikHeinrich.de>
2025-08-03 01:00:48 +0200

    refactor(ClientModTemplate): remove unused PacketType import

--- src/main/java/modtemplate/ClientModTemplate.java
+++ src/main/java/modtemplate/ClientModTemplate.java
@@ -6,7 +6,6 @@ import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
 import net.fabricmc.fabric.api.networking.v1.PayloadTypeRegistry;
 #else
-import net.fabricmc.fabric.api.networking.v1.PacketType;
 import net.fabricmc.fabric.api.networking.v1.PacketByteBufs;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
 import net.minecraft.network.FriendlyByteBuf;
