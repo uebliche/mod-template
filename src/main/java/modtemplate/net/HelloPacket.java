6fb9367fdbd91e82221965c1997d141fb892fdc8
Freddi <mail@FrederikHeinrich.de>
2025-08-02 20:46:20 +0200

    refactor(build): improve Minecraft version parsing logic

--- src/main/java/modtemplate/net/HelloPacket.java
+++ src/main/java/modtemplate/net/HelloPacket.java
@@ -15,7 +15,11 @@ public record HelloPacket(
             ByteBufCodecs.STRING_UTF8, HelloPacket::message,
             HelloPacket::new);
 
+    #if MC_VER >= MC_1_21
     public static final Type<HelloPacket> ID = new Type<>(ResourceLocation.parse("template:hello"));
+    #else
+    public static final Type<HelloPacket> ID = new Type<>(ResourceLocation.tryParse("template:hello"));
+    #endif
 
     @Override
     public @NotNull Type<? extends CustomPacketPayload> type() {
