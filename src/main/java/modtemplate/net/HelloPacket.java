de20a3833e92b4ad58a7b31e5f90530b3ba00b97
Freddi <mail@FrederikHeinrich.de>
2025-08-03 00:55:36 +0200

    refactor(HelloPacket): conditionally implement CustomPacketPayload based on MC version

--- src/main/java/modtemplate/net/HelloPacket.java
+++ src/main/java/modtemplate/net/HelloPacket.java
@@ -5,22 +5,27 @@ import net.minecraft.network.RegistryFriendlyByteBuf;
 import net.minecraft.network.codec.ByteBufCodecs;
 import net.minecraft.network.codec.StreamCodec;
 #else
+
 import net.minecraft.network.FriendlyByteBuf;
 #endif
+#if MC_VER > MC_1_20_1
 import net.minecraft.network.protocol.common.custom.CustomPacketPayload;
+#endif
 import net.minecraft.resources.ResourceLocation;
 import org.jetbrains.annotations.NotNull;
 
 public record HelloPacket(
         String message
-) implements CustomPacketPayload {
+)
+        #if MC_VER > MC_1_20_1
+        implements CustomPacketPayload
+        #endif {
 
     #if MC_VER >= MC_1_20_5
     public static final StreamCodec<RegistryFriendlyByteBuf, HelloPacket> STREAM_CODEC = StreamCodec.composite(
             ByteBufCodecs.STRING_UTF8, HelloPacket::message,
             HelloPacket::new);
     #endif
-
     #if MC_VER >= MC_1_21
     public static final Type<HelloPacket> ID = new Type<>(ResourceLocation.parse("template:hello"));
     #elif MC_VER >= MC_1_20_5
@@ -35,12 +40,16 @@ public record HelloPacket(
         return ID;
     }
     #else
+    #if MC_VER > MC_1_20_1
     @Override
+    #endif
     public @NotNull ResourceLocation id() {
         return ID;
     }
 
+    #if MC_VER > MC_1_20_1
     @Override
+    #endif
     public void write(FriendlyByteBuf friendlyByteBuf) {
         friendlyByteBuf.writeUtf(message);
     }
